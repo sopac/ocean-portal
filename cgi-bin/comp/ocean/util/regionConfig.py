31b56c5224c9b43f43444edf3a20abb748bcd0cf
Sheng Guo <s.guo@bom.gov.au>
2012-05-07 07:32:28 +1000

    Add util sub-package.

---
+++ cgi-bin/comp/ocean/util/regionConfig.py
@@ -0,0 +1,172 @@
+#
+#
+#
+#
+
+regions = {"pi": ("pi",
+                  {"llcrnrlat": -26,
+                   "llcrnrlon": 134,
+                   "urcrnrlat": 23,
+                   "urcrnrlon": 214
+                   }),
+           "cook": ("pi",
+                  {"llcrnrlat": -23,
+                   "llcrnrlon": 193,
+                   "urcrnrlat": -8,
+                   "urcrnrlon": 203
+                  }),
+           "fsm": ("pi", 
+                  {"llcrnrlat": ,
+                   "llcrnrlon": ,
+                   "urcrnrlat": ,
+                   "urcrnrlon": 
+                  }),
+           "fiji": ("pi", 
+                  {"llcrnrlat": -20,
+                   "llcrnrlon": 176.5,
+                   "urcrnrlat": -15,
+                   "urcrnrlon": 183
+                  }),
+           "kiribati": ("pi",
+                  {"llcrnrlat": -12,
+                   "llcrnrlon": 169,
+                   "urcrnrlat": 5,
+                   "urcrnrlon": 210
+                  }),
+           "marshall": ("pi",
+                  {"llcrnrlat": ,
+                   "llcrnrlon": ,
+                   "urcrnrlat": ,
+                   "urcrnrlon": 
+                  }),
+           "nauru": ("pi", 
+                  {"llcrnrlat": ,
+                   "llcrnrlon": ,
+                   "urcrnrlat": ,
+                   "urcrnrlon": 
+                  }),
+           "niue": ("pi",
+                  {"llcrnrlat": -20,
+                   "llcrnrlon": 189,
+                   "urcrnrlat": -18,
+                   "urcrnrlon": 191
+                  }),
+           "palau": ("pi", 
+                  {"llcrnrlat": ,
+                   "llcrnrlon": ,
+                   "urcrnrlat": ,
+                   "urcrnrlon": 
+                  }),
+           "png": ("pi",
+                  {"llcrnrlat": -12,
+                   "llcrnrlon": 128,
+                   "urcrnrlat": 1,
+                   "urcrnrlon": 160
+                  }),
+           "samoa": ("pi",
+                  {"llcrnrlat": -15,
+                   "llcrnrlon": 186,
+                   "urcrnrlat": -13,
+                   "urcrnrlon": 189
+                  }),
+           "solomon": ("pi", 
+                  {"llcrnrlat": -12,
+                   "llcrnrlon": 155,
+                   "urcrnrlat": -4.8,
+                   "urcrnrlon": 168
+                  }),
+           "tonga": ("pi", 
+                  {"llcrnrlat": -22,
+                   "llcrnrlon": 184,
+                   "urcrnrlat": -15,
+                   "urcrnrlon": 187
+                  }),
+           "tuvalu": ("pi", 
+                  {"llcrnrlat": -11,
+                   "llcrnrlon": 177,
+                   "urcrnrlat": -7,
+                   "urcrnrlon": 180
+                  }),
+           "vanuatu": ("pi",
+                  {"llcrnrlat": -21,
+                   "llcrnrlon": 166,
+                   "urcrnrlat": -13,
+                   "urcrnrlon": 170
+                  }),
+           "aus": ("aus",
+                  {"llcrnrlat": -50,
+                   "llcrnrlon": 60,
+                   "urcrnrlat": 20,
+                   "urcrnrlon": 170
+                  }),
+           "glob": ("aus",
+                  {"llcrnrlat": -90,
+                   "llcrnrlon": 0,
+                   "urcrnrlat": 90,
+                   "urcrnrlon": 360
+                  }),
+           "pac": ("aus",
+                  {"llcrnrlat": -45,
+                   "llcrnrlon": 100,
+                   "urcrnrlat": 45,
+                   "urcrnrlon": 300
+                  }),
+           "ind": ("aus",
+                  {"llcrnrlat": -45,
+                   "llcrnrlon": 20,
+                   "urcrnrlat": 45,
+                   "urcrnrlon": 160
+                  }),
+           "ntr": ("aus",
+                  {"llcrnrlat": -22,
+                   "llcrnrlon": 94,
+                   "urcrnrlat": 0,
+                   "urcrnrlon": 174
+                  }),
+           "sth": ("aus",
+                  {"llcrnrlat": -50,
+                   "llcrnrlon": 94,
+                   "urcrnrlat": -30,
+                   "urcrnrlon": 174
+                  }),
+           "tas": ("aus",
+                  {"llcrnrlat": -46,
+                   "llcrnrlon": 150,
+                   "urcrnrlat": -26,
+                   "urcrnrlon": 174
+                  }),
+           "cor": ("aus",
+                  {"llcrnrlat": -26,
+                   "llcrnrlon": 142,
+                   "urcrnrlat": -4,
+                   "urcrnrlon": 174
+                  }),
+           "nws": ("aus",
+                  {"llcrnrlat": -22,
+                   "llcrnrlon": 94,
+                   "urcrnrlat": -4,
+                   "urcrnrlon": 130
+                  }),
+           "swr": ("aus",
+                  {"llcrnrlat": -46,
+                   "llcrnrlon": 94,
+                   "urcrnrlat": -22,
+                   "urcrnrlon": 116
+                  }),
+           "eca": ("aus",
+                  {"llcrnrlat": -50,
+                   "llcrnrlon": 140,
+                   "urcrnrlat": 0,
+                   "urcrnrlon": 180
+                  }),
+           "wca": ("aus",
+                  {"llcrnrlat": -50,
+                   "llcrnrlon": 90,
+                   "urcrnrlat": 0,
+                   "urcrnrlon": 130
+                  }),
+           "sh": ("aus",
+                  {"boundinglat": -10,
+                   "lon0": 0
+                  })
+          }
