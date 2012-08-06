e4339acf2dc959adab0252659613b07a2a677385
Elisabeth Thompson <elisabeth.thompson@bom.gov.au>
2012-08-06 16:36:08 +1000

    removal of redundant script and renaming of correct script
    Signed-off-by: Elisabeth Thompson <elisabeth.thompson@bom.gov.au>

--- cgi-bin/comp/ocean/bran/bran.py
+++ cgi-bin/comp/ocean/bran/bran.py
@@ -10,6 +10,7 @@ from ..util import serverConfig
 
 #Maybe move these into configuration later
 branGraph = "%s_%s_%s_%s"
+branSubsurfaceGraph = "%s_%s_%s_%+07.2f_%+07.2f_%+07.2f_%+07.2f"
 avebranGraph = "%s_%s_%s_%save"
 decbranGraph = "%s_%s_%sdec.png"
 
@@ -17,10 +18,10 @@ decbranGraph = "%s_%s_%sdec.png"
 serverCfg = serverConfig.servers[serverConfig.currentServer]
 
 #get dataset dependant production information
-branProduct = productName.products["blue_link"]
+branProduct = productName.products["bran"]
 
 #get the plotter
-plotter = branPlotter.branPlotter()
+plotter = branPlotter.BranPlotter()
 
 def process(form):
     responseObj = {} #this object will be encoded into a json string
@@ -30,128 +31,127 @@ def process(form):
         areaStr = form["area"].value
         periodStr = form["period"].value
 
-        args = {"variable": mapStr,
+        args = {"var": mapStr,
                 "date": dateStr,
                 "area": areaStr,
                 "period": periodStr}
-
-
-        if mapStr == 'anom':
-            if "average" in form and form["average"].value == "true":
-                dateStr = dateStr[4:6] #extract month value
-                if form["trend"].value == "true":
-                    if periodStr == 'monthly':
-                        responseObj["aveImg"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["monthly"]\
-                                              + "_%s_ave_%s_trend.png" % (dateStr, areaStr)
-                        responseObj["aveData"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["monthly"]\
-                                              + '_%s_ave_%s.txt"' % (dateStr, areaStr)
-                        responseObj["mean"] = str(areaMean.monthlyMean[dateStr][areaStr])
-                    elif periodStr == 'yearly':
-                        responseObj["aveImg"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["yearlyAve"]\
-                                              + '_ave_%s_trend.png"' % (areaStr)
-                        responseObj["aveData"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["yearlyAve"] \
-                                              + '"_ave_%s.txt"' % (areaStr)
-                        responseObj["mean"] = str(areaMean.yearlyMean[areaStr])
-                elif form["runningAve"].value == "true":
-                    if periodStr == 'monthly':
-                        if "runningInterval" in form:
-                            runningInterval = int(form["runningInterval"].value)
-                            responseObj["aveImg"] = serverCfg["baseURL"]\
-                                                  + serverCfg["cacheDir"]["blue_link"]\
-                                                  + branProduct["monthlyAve"]\
-                                                  + '_%s_ave_%s_%02dmoving.png"' % (dateStr, areaStr, runningInterval)
-                            responseObj["aveData"] = serverCfg["baseURL"]\
-                                                   + serverCfg["cacheDir"]["blue_link"]\
-                                                   + branProduct["monthlyAve"]\
-                                                   + '_%s_ave_%s.txt"' % (dateStr, areaStr)
-                            responseObj["mean"] = str(areaMean.monthlyMean[dateStr][areaStr])
-                    elif periodStr == 'yearly':
-                        responseObj["aveImg"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["yearly"]\
-                                              + '_ave_%s_%02dmoving.png"' % (areaStr, runningInterval)
-                        responseObj["aveData"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["yearly"]\
-                                              + '_ave_%s.txt' % (areaStr)
-                        responseObj["mean"] = str(areaMean.yearlyMean[areaStr])
-                else:
-                    if periodStr == 'monthly':
-                        responseObj["aveImg"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["monthlyAve"]\
-                                              + '_%s_ave_%s.png"' % (dateStr, areaStr)
-                        responseObj["aveData"] = serverCfg["baseURL"]\
-                                               + serverCfg["cacheDir"]["blue_link"]\
-                                               + branProduct["monthlyAve"]\
-                                               + '_%s_ave_%s.txt"' % (dateStr, areaStr)
-                        responseObj["mean"] = str(areaMean.monthlyMean[dateStr][areaStr])
-                    elif periodStr == 'yearly':
-                        responseObj["aveImg"] = serverCfg["baseURL"]\
-                                              + serverCfg["cacheDir"]["blue_link"]\
-                                              + branProduct["yearlyAve"]\
-                                              + '_ave_%s.png"' % (areaStr)
-                        responseObj["aveData"] = serverCfg["baseURL"]\
-                                               + serverCfg["cacheDir"]["blue_link"]\
-                                               + branProduct["yearlyAve"]\
-                                               + '_ave_%s.txt"' % (areaStr)
-                        responseObj["mean"] = str(areaMean.yearlyMean[areaStr])
-        elif mapStr == "dec":
-            fileName = decGraph % (branProduct["monthlyDec"], areaStr, dateStr[:6])
-            outputFileName = serverCfg["outputDir"] + fileName
-            if not os.path.exists(outputFileName + ".png"):
-                plotter.plot(fileName, **args)
-            if not os.path.exists(outputFileName + ".png"):
-                responseObj["error"] = "Requested image is not available at this time."
-            else:
-                responseObj["img"] = serverCfg["baseURL"]\
-                                   + outputFileName + ".png"
-                responseObj["mapeast"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_east.png"
-                responseObj["mapeastw"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_east.pgw"
-                responseObj["mapwest"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_west.png"
-                responseObj["mapwestw"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_west.pgw"
-        else:
         ####current xml html response
-            if periodStr == 'daily':
-                fileName = branGraph % (branProduct["daily"], mapStr, areaStr, dateStr)
-            elif periodStr == 'monthly':
-                fileName = branGraph % (branProduct["monthly"], mapStr, areaStr, dateStr[:6])
-            elif periodStr == 'yearly':
-                fileName = avebranGraph % (branProduct["yearly"], mapStr, areaStr, dateStr[:4])
-            elif periodStr == '3monthly':
-                fileName = avebranGraph % (branProduct["3monthly"], mapStr, areaStr, dateStr[:6])
-            elif periodStr == '6monthly':
-                fileName = avebranGraph % (branProduct["6monthly"], mapStr, areaStr, dateStr[:6])
-            elif periodStr == 'weekly':
-                fileName = avebranGraph % (branProduct["weekly"], mapStr, areaStr, dateStr)
+        if periodStr == 'daily':
+            fileName = branGraph % (branProduct["daily"], mapStr, areaStr, dateStr)
+        elif periodStr == 'monthly':
+            fileName = branGraph % (branProduct["monthly"], mapStr, areaStr, dateStr[:6])
+        elif periodStr == 'yearly':
+            fileName = branGraph % (branProduct["yearly"], mapStr, areaStr, dateStr[:4])
+        elif periodStr == '3monthly':
+            fileName = branGraph % (branProduct["3monthly"], mapStr, areaStr, dateStr[:6])
+        elif periodStr == '6monthly':
+            fileName = branGraph % (branProduct["6monthly"], mapStr, areaStr, dateStr[:6])
+        elif periodStr == 'weekly':
+            fileName = branGraph % (branProduct["weekly"], mapStr, areaStr, dateStr)
+        outputFileName = serverCfg["outputDir"] + fileName
+#        if not os.path.exists(outputFileName + ".png"):
+#            plotter.plot(fileName, mapStr, dateStr, areaStr, periodStr)
+#        if not os.path.exists(outputFileName + ".png"):
+#            responseObj["error"] = "Requested image is not available at this time."
+#        else:
+#            responseObj["img"] = serverCfg["baseURL"]\
+#                               + outputFileName + ".png"
+#            responseObj["mapeast"] = serverCfg["baseURL"]\
+#                                   + outputFileName + "_east.png"
+#            responseObj["mapeastw"] = serverCfg["baseURL"]\
+#                                   + outputFileName + "_east.pgw"
+#            responseObj["mapwest"] = serverCfg["baseURL"]\
+#                                   + outputFileName + "_west.png"
+#            responseObj["mapwestw"] = serverCfg["baseURL"]\
+#                                   + outputFileName + "_west.pgw"
+
+    if "xlat1" in form and "xlon1" in form and "xlon2" in form:
+        xlat1 = form["xlat1"].value
+        xlon1 = form["xlon1"].value
+        xlat2 = form["xlat2"].value
+        xlon2 = form["xlon2"].value
+        args['xlat1']=xlat1
+        args['xlon1']=xlon1
+        args['xlat2']=xlat2
+        args['xlon2']=xlon2
+
+        #plot subsurface
+        if periodStr == 'monthly':
+            fileName = branSubsurfaceGraph % (branProduct["monthly"], mapStr, dateStr[:6],\
+                                              float(xlat1), float(xlon1), float(xlat2), float(xlon2))
             outputFileName = serverCfg["outputDir"] + fileName
             if not os.path.exists(outputFileName + ".png"):
-                plotter.plot(fileName, mapStr, dateStr, areaStr, periodStr)
+                plotter.plotSubsurface(fileName, **args)
             if not os.path.exists(outputFileName + ".png"):
                 responseObj["error"] = "Requested image is not available at this time."
             else:
                 responseObj["img"] = serverCfg["baseURL"]\
-                                   + outputFileName + ".png"
-                responseObj["mapeast"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_east.png"
-                responseObj["mapeastw"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_east.pgw"
-                responseObj["mapwest"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_west.png"
-                responseObj["mapwestw"] = serverCfg["baseURL"]\
-                                       + outputFileName + "_west.pgw"
-
+                               + outputFileName + ".png"
+#            var = "temp_ano"
+#            args["variable"] = var
+#            fileName = seaChart % (seaLevelProduct["monthly"], tidalGaugeId, var)
+#            outputFileName = serverCfg["outputDir"] + fileName
+#            if not os.path.exists(outputFileName + ".png"):
+#                plotter.plotTidalGauge(outputFileName, **args)
+#            if not os.path.exists(outputFileName + ".png"):
+#                responseObj["error"] = "Requested image is not available at this time."
+#            else:
+#                responseObj["img"].append(serverCfg["baseURL"]\
+#                            + outputFileName + ".png")
+#                responseObj["tid"] = serverCfg["baseURL"]\
+#                            + outputFileName + ".txt"
+#            var = "temp_dec"
+#            args["variable"] = var
+#            fileName = seaChart % (seaLevelProduct["monthly"], tidalGaugeId, var)
+#            outputFileName = serverCfg["outputDir"] + fileName
+#            if not os.path.exists(outputFileName + ".png"):
+#                plotter.plotTidalGauge(outputFileName, **args)
+#            if not os.path.exists(outputFileName + ".png"):
+#                responseObj["error"] = "Requested image is not available at this time."
+#            else:
+#                responseObj["img"].append(serverCfg["baseURL"]\
+##                                   + outputFileName + ".png")
+#                responseObj["tid"] = serverCfg["baseURL"]\
+#                                   + outputFileName + ".txt"
+#            var = "salt"
+#            args["variable"] = var
+#            fileName = seaChart % (seaLevelProduct["monthly"], tidalGaugeId, var)
+#            outputFileName = serverCfg["outputDir"] + fileName
+#            if not os.path.exists(outputFileName + ".png"):
+#                plotter.plotTidalGauge(outputFileName, **args)
+#            if not os.path.exists(outputFileName + ".png"):
+#                responseObj["error"] = "Requested image is not available at this time."
+#            else:
+#                responseObj["img"].append(serverCfg["baseURL"]\
+#                                   + outputFileName + ".png")
+#                responseObj["tid"] = serverCfg["baseURL"]\
+#                                   + outputFileName + ".txt"
+#            var = "eta"
+#            args["variable"] = var
+#            fileName = seaChart % (seaLevelProduct["monthly"], tidalGaugeId, var)
+#            outputFileName = serverCfg["outputDir"] + fileName
+#            if not os.path.exists(outputFileName + ".png"):
+#                plotter.plotTidalGauge(outputFileName, **args)
+#            if not os.path.exists(outputFileName + ".png"):
+#                responseObj["error"] = "Requested image is not available at this time."
+#            else:
+#                responseObj["img"].append(serverCfg["baseURL"]\
+#                                   + outputFileName + ".png")
+#                responseObj["tid"] = serverCfg["baseURL"]\
+#                                   + outputFileName + ".txt"
+#            var = "uv"
+#            args["variable"] = var
+#            fileName = seaChart % (seaLevelProduct["monthly"], tidalGaugeId, var)
+#            outputFileName = serverCfg["outputDir"] + fileName
+#            if not os.path.exists(outputFileName + ".png"):
+#                plotter.plotTidalGauge(outputFileName, **args)
+#            if not os.path.exists(outputFileName + ".png"):
+#                responseObj["error"] = "Requested image is not available at this time."
+#            else:
+#                responseObj["img"].append(serverCfg["baseURL"]\
+#                                   + outputFileName + ".png")
+#                responseObj["tid"] = serverCfg["baseURL"]\
+#                                   + outputFileName + ".txt"
+#
     response = json.dumps(responseObj)
     return response
