sopac/ocean-portal

commit b7acc3e208b97576963dc79721fe7053a694c282

compmap: wrap compmap.js script in $() so it can go in the header

js/comp/compmap.js
@@ -1,4 +1,5 @@
 /*jslint eqeq: true, undef: true, sloppy: true, sub: true, todo: true, vars: true, white: true, browser: true, windows: true */
+$(function() {
 
 //    OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/";
     var ocean = ocean || {};
@@ -385,4 +386,5 @@ Ext.onReady(function() {
 
 
     setupControls();
+  });
 });

html/compmap.html
@@ -13,6 +13,7 @@
 <script type="text/javascript" src="lib/jquery/jquery.datepick.ext.js"></script>
 
 <script type="text/javascript" src="js/comp/controlPanel.js"></script>
+<script type="text/javascript" src="js/comp/compmap.js"></script>
 
 <link rel="stylesheet" type="text/css" href="lib/ext/resources/css/ext-all.css" />
 <link rel="stylesheet" type="text/css" href="lib/jquery/css/smoothness/jquery-ui-1.8.22.custom.css"/>
@@ -89,7 +90,5 @@
       </form>
 </div>
 
-<!-- script must go at end -->
-<script type="text/javascript" src="js/comp/compmap.js"></script>
 </body> 
 </html>

commit a2d7b5a6c6b6f9459751c9a945c19dd5d3c10142

compmap: clean up mess caused by b7acc3e208b9757696

js/comp/compmap.js
@@ -1,10 +1,9 @@
 /*jslint eqeq: true, undef: true, sloppy: true, sub: true, todo: true, vars: true, white: true, browser: true, windows: true */
-$(function() {
+var ocean = ocean || {};
+var map;
 
-//    OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/";
-    var ocean = ocean || {};
-    var popup;
-    var map = new OpenLayers.Map("map", {
+$(document).ready(function() {
+    map = new OpenLayers.Map("map", {
         resolutions: [0.087890625,0.0439453125,0.02197265625,0.010986328125,0.0054931640625,0.00274658203125,0.00137329101],
         maxResolution: 0.087890625, 
         minExtent: new OpenLayers.Bounds(-1, -1, -1, -1),
@@ -63,14 +62,6 @@ $(function() {
 //            maxResolution: 0.0000453613
         });
 
-    function centerMap() {
-        if (map) {
-//            map.setCenter(new OpenLayers.LonLat(-178.48551, -13.11418), 0);
-            map.setCenter(new OpenLayers.LonLat(173.00000, -13.11418), 0);
-//            gaugesLayer.setVisibility(true);
-        }
-    }
-    
     //Add gauge points
     map.addLayers([bathymetryLayer]);
     map.setBaseLayer(bathymetryLayer);
@@ -100,6 +91,7 @@ $(function() {
             }
         }
     } 
+});
 
 function selectCountry(event, args) {
     var selection = event.getValue();
@@ -117,6 +109,12 @@ function setupControls() {
     window.countryCombo.on('select', selectCountry, this);
     window.countryCombo.on('change', selectCountry, this);
 //    window.countryCombo.select('pac');
+}
+
+function centerMap() {
+    if (map) {
+        map.setCenter(new OpenLayers.LonLat(173.00000, -13.11418), 0);
+    }
 }
 
 function updateMap(layerName, data){
@@ -387,4 +385,3 @@ Ext.onReady(function() {
 
     setupControls();
   });
-});

js/comp/controlPanel.js
@@ -42,13 +42,13 @@ ocean.dsConf = {
                    + "&map=" + this.variable.get('id')
                    + "&date=" + $.datepick.formatDate('yyyymmdd', ocean.date)
                    + "&period=" + ocean.period
-//                   + "&area=aus"
                    + "&area=" + ocean.area
                    + "&average=" + ocean.dsConf['reynolds'].aveCheck.average
                    + "&trend=" + ocean.dsConf['reynolds'].aveCheck.trend
                    + "&runningAve=" + ocean.dsConf['reynolds'].aveCheck.runningAve
                    + "&runningInterval=" + ocean.dsConf['reynolds'].runningInterval
-                   + "&timestamp=" + new Date().getTime();},
+                   + "&timestamp=" + new Date().getTime();
+                },
                 data: null,
                 variable: null,
                 aveCheck: {},
@@ -143,7 +143,8 @@ ocean.dsConf = {
                    + "&trend=" + ocean.dsConf['ersst'].aveCheck.trend
                    + "&runningAve=" + ocean.dsConf['ersst'].aveCheck.runningAve
                    + "&runningInterval=" + ocean.dsConf['ersst'].runningInterval
-                   + "&timestamp=" + new Date().getTime();},
+                   + "&timestamp=" + new Date().getTime();
+                },
                 data: null,
                 variable: null,
                 aveCheck: {},