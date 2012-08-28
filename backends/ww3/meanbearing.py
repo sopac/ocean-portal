1fb9819b62c456850118e88c8900b0903316d8f4
Jason A. Smith <Jason.Smith@bom.gov.au>
2012-08-28 10:30:35 +1000

    Updated meanbearing module to function with int input types

--- backends/ww3/meanbearing.py
+++ backends/ww3/meanbearing.py
@@ -16,19 +16,17 @@ def meanbearing(wdir):
     wavedir = wdir  
     for i in range(len(wavedir)):
        #convert to radians
-       wavedir[i] = np.pi*(wavedir[i]/180.0)
+       a = float(wavedir[i])
+       a = a*((np.pi)/180.0)
        #disagreegate angles into x,y components
-       y  = y + np.sin(wavedir[i])
-       x  = x + np.cos(wavedir[i])
-    
-    #calculate the mean vector
-    meany = y/len(wavedir)
-    meanx = x/len(wavedir)
-    #calculate mean bearing by taking the inverse tangent of y/x
-    bearing = np.arctan2(meany,meanx)
-    
+       y = y + np.sin(a)
+       x = x + np.cos(a) 
+    #calculate mean bearing by taking the inverse tangent of y/xi
+    bearing = np.arctan2(y,x)
     #arctan2 returns range -pi,pi rather than 0,2pi, so conversion is required if < 0.
     if bearing < 0:
         bearing = bearing + 2*np.pi
+    
+    bearing = bearing*(180.0/(np.pi))
 
     return bearing
