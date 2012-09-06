7ecbf3148703f48f9bf99bd0e5d22f0bbc651728
Jason A. Smith <Jason.Smith@bom.gov.au>
2012-09-06 15:50:39 +1000

    more formatting updates to wavewatch III plot annotations.

--- backends/ww3/WavePlots.py
+++ backends/ww3/WavePlots.py
@@ -98,18 +98,18 @@ def RosePlot(opath, wdir, units, lat, lon, ilat, ilon, xstr, title, var, binwd):
     plt.figtext(0.12,0.92,'%s' % title, fontsize=16, weight=800)
     #various annotations to plot
     plt.figtext(0.76, 0.82,'Probabilities:', fontsize = 10, weight = 550)
-    plt.figtext(0.76, 0.575,'Location & Plot Data:', fontsize=10, weight =550)
+    plt.figtext(0.76, 0.575,'Location & Plot Data:', fontsize=10, weight = 550)
 
-    plt.figtext(0.76, 0.525, 'Input Lat/Lon: %s %s' % (formilat,formilon), fontsize=10)
-    plt.figtext(0.76, 0.5, 'Grid Lat/Lon: %s %s' % (formlat, formlon), fontsize=10)
+    plt.figtext(0.76, 0.51, 'Input Lat/Lon:\n %s %s' % (formilat,formilon), fontsize=10)
+    plt.figtext(0.76, 0.46, 'Grid Lat/Lon:\n %s %s' % (formlat, formlon), fontsize=10)
 
-    plt.figtext(0.76, 0.45, 'Data points: %s' % length, fontsize=10)
-    plt.figtext(0.76, 0.425, 'Bins: %s' % int(N), fontsize=10)
-    plt.figtext(0.76, 0.4, 'Bin Width: %s %s' % (binwd, degree), fontsize=10)
+    plt.figtext(0.76, 0.425, 'Data points: %s' % length, fontsize=10)
+    plt.figtext(0.76, 0.4, 'Bins: %s' % int(N), fontsize=10)
+    plt.figtext(0.76, 0.375, 'Bin Width: %s %s' % (binwd, degree), fontsize=10)
 
-    plt.figtext(0.76, 0.3, 'Mean True Bearing: %s %s' % (meanbr, degree), color='r', fontsize=10)
+    plt.figtext(0.76, 0.275, 'Mean True Bearing: %s %s' % (meanbr, degree), color='r', fontsize=10)
     #display wave direction percentages for 8 primary compass points in the windrose
-    plt.figtext(0.76, 0.35, 'Directional Statistics:', fontsize=10, weight=550)
+    plt.figtext(0.76, 0.325, 'Directional Statistics:', fontsize=10, weight=550)
     plt.figtext(0.76, 0.25, 'North: %s %s' % (perc[0], units), fontsize=10)
     plt.figtext(0.76, 0.225, 'North East: %s %s' % (perc[1], units), fontsize=10)
     plt.figtext(0.76, 0.2, 'East: %s %s' % (perc[2], units), fontsize=10)
@@ -229,22 +229,22 @@ def HistPlot(opath, wheight, units, lat, lon, ilat, ilon, xstr, title, var, binw
     #various annotations for graphics
     plt.figtext(0.79, 0.8, 'Distribution:', fontsize=10, weight=550)
     plt.figtext(0.79, 0.625, 'Location & Plot Data:',fontsize = 10, weight = 550)
-    plt.figtext(0.79, 0.575, 'Input Lat/Lon:\n %s %s' % (formilat,formilon), fontsize=10)
-    plt.figtext(0.79, 0.525, 'Grid Lat/Lon:\n %s %s' % (formlat,formlon), fontsize=10)
+    plt.figtext(0.79, 0.56, 'Input Lat/Lon:\n %s %s' % (formilat,formilon), fontsize=10)
+    plt.figtext(0.79, 0.51, 'Grid Lat/Lon:\n %s %s' % (formlat,formlon), fontsize=10)
 
     plt.figtext(0.79, 0.475, 'Data points: %s' % length ,fontsize=10)
     plt.figtext(0.79, 0.45, 'Bins: %s'  % int(binnum), fontsize=10)
     plt.figtext(0.79, 0.425, 'Bin Width: %s %s' % (binwd,units), fontsize=10)
-    plt.figtext(0.79, 0.4, 'Statistical Information:', fontsize=10, weight=550)
+    plt.figtext(0.79, 0.375, 'Statistical Information:', fontsize=10, weight=550)
 
-    plt.figtext(0.79, 0.3, 'Mean: %s %s' % (wavgr, units), color='r', fontsize=10)
-    plt.figtext(0.79, 0.35, 'Max: %s %s' % (maxwave, units), fontsize=10)
-    plt.figtext(0.79, 0.25, 'Min: %s %s' % (minwave, units), fontsize=10)
+    plt.figtext(0.79, 0.275, 'Mean: %s %s' % (round(wavgr,1), units), color='r', fontsize=10)
+    plt.figtext(0.79, 0.325, 'Max: %s %s' % (round(maxwave,1), units), fontsize=10)
+    plt.figtext(0.79, 0.225, 'Min: %s %s' % (round(minwave,1), units), fontsize=10)
 
-    plt.figtext(0.79, 0.275,'25th Percentile: %s %s' % (q1,units), fontsize=10)
-    plt.figtext(0.79, 0.325,'75th Percentile: %s %s' % (q3,units), fontsize=10)
+    plt.figtext(0.79, 0.25,'25th Percentile: %s %s' % (round(q1,1),units), fontsize=10)
+    plt.figtext(0.79, 0.3,'75th Percentile: %s %s' % (round(q3,1),units), fontsize=10)
     #Bureau of Meteorology Copyright
-    plt.figtext(0.65, 0.03, u'WAVEWATCH III$^{\u00AE}$', fontsize=10)
+    plt.figtext(0.68, 0.03, u'WAVEWATCH III$^{\u00AE}$', fontsize=10)
     plt.figtext(0.02, 0.02, getCopyright(), fontsize=8)
      #define image name
     imgname = opath + '.png'
