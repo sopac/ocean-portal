a727dec7916f770ccf63a741734a83bc9ae110f3
Danielle Madeley <d.madeley@bom.gov.au>
2012-09-26 14:24:48 +1000

    build_py: Don't continuously write versions to files
    Make it more resiliant, only write files when they need writing.

--- localdistutils/build_py.py
+++ localdistutils/build_py.py
@@ -5,11 +5,23 @@
 #
 # Authors: Danielle Madeley <d.madeley@bom.gov.au>
 
+import os
+
 from distutils.command.build_py import build_py as real_build_py
+from distutils import log
 
 import util
 
 ver = util.get_version()
+
+LOOK_BACK = 100 # chars
+
+def try_and_seek(f, offset):
+    try:
+        f.seek(offset, os.SEEK_END)
+    except IOError:
+        # file too short
+        pass
 
 class build_py(real_build_py):
 
@@ -20,6 +32,34 @@ class build_py(real_build_py):
         package = package.split('.')
         outfile = self.get_module_outfile(self.build_lib, package, module)
 
-        with open(outfile, 'a') as module:
+        with open(outfile, 'r+U') as module:
+            verstr = "__version__ = '%s'" % (ver)
+
+            try_and_seek(module, -LOOK_BACK)
+
+            # do we have a version string?
+            line = module.read()
+            idx = line.rfind('\n__version__')
+
+            if idx != -1:
+                # is it the right version string?
+                idx = -len(line) + idx
+
+                try_and_seek(module, idx)
+                line = module.read().strip()
+
+                if line == verstr:
+                    # all good in the hood
+                    return
+                else:
+                    try_and_seek(module, idx)
+                    module.truncate()
+            else:
+                # no version string found
+                pass
+
+            # write the version string
+            log.info('writing version: %s' % outfile)
+            try_and_seek(module, 0)
             print >> module
-            print >> module, "__version__ = '%s'" % (ver)
+            print >> module, verstr
