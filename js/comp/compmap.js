a801832b69800c94992330cdd0df2f05382fd358
Danielle Madeley <d.madeley@bom.gov.au>
2012-10-11 13:05:34 +1100

    Port region selected combo box from ExtJS to jQuery
    This means once regions.py implements caching, we can cache the *entire*
    portal load. It will also allow flexility to do things like group
    regions together.
    
    This means there are no form controls provided by ExtJS, all that ExtJS
    now provides is the panels.

--- js/comp/compmap.js
+++ js/comp/compmap.js
@@ -40,6 +40,23 @@ $(document).ready(function() {
 
     createMap();
 
+    $('#region').change(function () {
+        var selected = $('#region option:selected');
+
+        if (selected.length == 0) {
+            return;
+        }
+
+        var bounds = selected.data('bounds');
+
+        console.log(bounds);
+
+        map.setCenter(bounds.getCenterLonLat(),
+                      map.getZoomForExtent(bounds));
+
+        ocean.area = selected.val();
+    });
+
     /* request the portals config */
     $.getJSON('config/comp/portals.json')
         .success(function(data, status_, xhr) {
@@ -63,7 +80,15 @@ $(document).ready(function() {
 
             /* iterate the region bounds to calculate the restricted extent */
             $.each(data, function (i, region) {
-                bounds.extend(new OpenLayers.Bounds(region.extent));
+                var b = new OpenLayers.Bounds(region.extent);
+
+                bounds.extend(b);
+
+                $('<option>', {
+                    value: region.abbr,
+                    text: region.name
+                }).data('bounds', b)
+                  .appendTo('#region');
             });
 
             /* compensate for the date line wrapping */
@@ -75,6 +100,7 @@ $(document).ready(function() {
             console.log(bounds);
 
             map.setOptions({ restrictedExtent: bounds });
+            setValue('region', ocean.config);
         })
         .error(function (xhr, status_, error) {
             fatal_error("Error loading portals config " + "&mdash; " + error);
@@ -417,64 +443,6 @@ function enlargeImg(img, show) {
 
 Ext.require(['*']);
 Ext.onReady(function() {
-    var countryCombo, countryStore;
-    var countrylisturl = 'cgi/regions.py?portal=' + ocean.config;
-
-    Ext.define('Country', {
-        extend: 'Ext.data.Model',
-        fields: ['name', 'abbr', 'extent'],
-        idProperty: 'abbr',
-        proxy: {
-            type: 'ajax',
-            url: countrylisturl,
-            reader: {
-                type: 'json'
-            }
-        }
-    });
-
-    countryStore = new Ext.data.Store({
-        autoLoad: true,
-        model: 'Country',
-        listeners: {
-            load: function () {
-                countryCombo.select(ocean.config);
-            }
-        }
-    });
-
-    function _selectCountry(event, args) {
-        var selection = event.getValue();
-        var record = countryStore.getById(selection);
-
-        if (!record)
-            return;
-
-        var bounds = new OpenLayers.Bounds(record.get('extent'));
-
-        console.log(bounds);
-
-        map.setCenter(bounds.getCenterLonLat(),
-                      map.getZoomForExtent(bounds));
-
-        ocean.area = selection;
-    }
-
-    countryCombo = Ext.create('Ext.form.field.ComboBox', {
-        fieldLabel: 'Select a country/region',
-        labelAlign: 'top',
-        displayField: 'name',
-        valueField: 'abbr',
-        store: countryStore,
-        queryMode: 'local',
-        padding: 5,
-        height: '60%',
-        width: 180,
-        listeners: {
-            change: _selectCountry
-        }
-    });
-
     Ext.create('Ext.Viewport', {
         layout: {
             type: 'border',
@@ -483,7 +451,6 @@ Ext.onReady(function() {
         items: [{
             xtype: 'panel',
             region: 'west',
-            id: 'westDiv',
             collapsible: true,
             title: 'Parameters',
             width: 225,
@@ -492,7 +459,7 @@ Ext.onReady(function() {
             items: [{
                 xtype: 'panel',
                 region: 'north',
-                items: [countryCombo]
+                contentEl: 'regionPanel'
             }, {
                 xtype: 'panel',
                 region: 'center',
