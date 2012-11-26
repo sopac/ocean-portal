f2c9e9584b8fff73f54466f0822ae8aefa2b4300
Danielle Madeley <d.madeley@bom.gov.au>
2012-11-26 14:43:56 +1100

    tests: improved retry reporting

--- ocean/tests/conftest.py
+++ ocean/tests/conftest.py
@@ -6,7 +6,7 @@
 # Authors: Danielle Madeley <d.madeley@bom.gov.au>
 
 import pytest
-from _pytest.runner import call_and_report
+from _pytest.runner import call_and_report, BaseReport
 
 def pytest_generate_tests(__multicall__, metafunc):
     """
@@ -31,7 +31,8 @@ def pytest_runtest_protocol(item, nextitem):
 
     # the unstable marker means we should try a test several times
     if 'unstable' in item.keywords:
-        retries = item.keywords['unstable'].kwargs.get('retries', 3)
+        tries = item.keywords['unstable'].kwargs.get('tries', 3)
+        assert tries > 0
 
         item.ihook.pytest_runtest_logstart(nodeid=item.nodeid,
                                            location=item.location)
@@ -39,11 +40,59 @@ def pytest_runtest_protocol(item, nextitem):
         rep = call_and_report(item, 'setup')
 
         if rep.passed:
-            for i in xrange(retries):
-                rep = call_and_report(item, 'call')
+            reports = []
+            for i in xrange(tries):
+                reports.append(call_and_report(item, 'call', log=False))
                 if rep.passed:
                     break
+
+            item.ihook.pytest_runtest_logreport(report=UnifiedReport(reports))
 
         call_and_report(item, 'teardown', nextitem=nextitem)
 
         return True
+
+class UnifiedReport(BaseReport):
+    """
+    A collection of reports, as used by the 'unstable' marker
+    """
+
+    def __init__(self, reports=[]):
+        self.reports = reports
+
+    def toterminal(self, out):
+        for r in self.reports:
+            r.toterminal(out)
+
+    @property
+    def passed(self):
+        return any([r.passed for r in self.reports])
+
+    @property
+    def failed(self):
+        return all([r.failed for r in self.reports])
+
+    @property
+    def skipped(self):
+        return all([r.skipped for r in self.reports])
+
+    @property
+    def outcome(self):
+        if self.skipped:
+            return 'skipped'
+        elif self.failed:
+            return 'failed'
+        else:
+            return 'passed'
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return BaseReport.__getattr__(self, attr)
+
+        f = lambda r: getattr(r, attr)
+        v = f(self.reports[0])
+
+        # sanity check
+        assert all([v == f(r) for r in self.reports[1:]])
+
+        return v
