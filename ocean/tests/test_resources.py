3282933e3ac38c9e30772ed569775cabcd9e6c9d
Danielle Madeley <d.madeley@bom.gov.au>
2012-12-12 15:55:07 +1100

    Add a basic JSON lint

--- ocean/tests/test_resources.py
+++ ocean/tests/test_resources.py
@@ -7,6 +7,9 @@
 
 import os.path
 import subprocess
+import json
+
+import pytest
 
 import ocean
 
@@ -14,6 +17,24 @@ def get_resource(*args):
     return os.path.join(ocean.__path__[0], '..', *args)
 
 def test_html_validates():
-    # FIXME: find this file on the disk more reliably
+    """
+    This test just applies a basic lint, it doesn't yet check for conformance.
+    """
+
     path = get_resource('html', 'compmap.html')
     subprocess.check_call(['xmllint', path])
+
+@pytest.mark.parametrize(('file'), [
+    ('datasets.json'),
+    ('portals.json'),
+    ('vargroups.json'),
+])
+def test_json_validates(file):
+    """
+    This test just tries to load the JSON file with the Python JSON module.
+    It doesn't run a proper lint or check for JSON conformance.
+    """
+
+    path = get_resource('config', 'comp', file)
+    with open(path, 'r') as f:
+        json.load(f)
