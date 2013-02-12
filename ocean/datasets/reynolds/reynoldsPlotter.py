63692f4c63b70b2e2c66bcf25c895cc3066327fd
Danielle Madeley <d.madeley@bom.gov.au>
2013-02-12 15:57:43 +1100

    Port reynoldsPlotter to Gridset
    Next step is to further abstract this into a SurfacePlotter

--- ocean/datasets/reynolds/reynoldsPlotter.py
+++ ocean/datasets/reynolds/reynoldsPlotter.py
@@ -4,15 +4,18 @@
 #     All Rights Reserved
 #
 # Authors: Sheng Guo <s.guo@bom.gov.au>
+#          Danielle Madeley <d.madeley@bom.gov.au>
+
+import os.path
 
 import numpy as np
 import bisect
 
 from ocean import util, config
 from ocean.config import regionConfig
-from ocean.util import dateRange
+from ocean.util import dateRange, Parameterise
 from ocean.netcdf import plotter
-from ocean.datasets.bran import branPlotterNew
+from ocean.netcdf.grid import Gridset
 
 import reynoldsConfig as rc
 import reynoldsSpatialMean as spatialMean
@@ -22,75 +25,84 @@ class ReynoldsPlotter ():
     Reynolds plotter is specifically designed to plot the reynolds
     netcdf data
     """
-    config = None
-    serverCfg = None
+
+    pp = Parameterise()
 
     def __init__(self):
-       """Does nothing""" 
        self.config = rc.ReynoldsConfig()
        self.serverCfg = config.get_server_config()
+
+    # --- get_path ---
+    @pp.apply_to()
+    def get_path(self, params={}):
+        return os.path.join(self.serverCfg['dataDir']['reynolds'],
+                            'averages', params['period'])
+
+    @pp.apply_to(period='daily')
+    def get_path(self, params={}):
+        return os.path.join(self.serverCfg['dataDir']['reynolds'],
+                            'daily-new-uncompressed' )
+
+    @pp.apply_to(variable='dec')
+    def get_path(self, params={}):
+        return os.path.join(self.serverCfg['dataDir']['reynolds'],
+                            'decile', '1950', params['period'])
+
+    # --- get_prefix ---
+    @pp.apply_to(period='daily')
+    def get_prefix(self, params={}):
+        return 'avhrr-only-v2.'
+
+    @pp.apply_to()
+    def get_prefix(self, params={}):
+        return 'reynolds_sst_avhrr-only-v2_'
+
+    # --- get_suffix ---
+    @pp.apply_to(variable='dec')
+    def get_suffix(self, params={}):
+        return '_dec.nc'
+
+    @pp.apply_to()
+    def get_suffix(self, params={}):
+        return '.nc'
+
+    # --- get_formatted_date ---
+    @pp.apply_to(period='daily')
+    def get_formatted_date(self, params={}):
+        return params['date'].strftime('%d %B %Y')
+
+    @pp.apply_to(period='monthly')
+    def get_formatted_date(self, params={}):
+        return params['date'].strftime('%B %Y')
+
+    @pp.apply_to(period='3monthly')
+    def get_formatted_date(self, params={}):
+        return self._get_formatted_date(params, 3)
+
+    @pp.apply_to(period='6monthly')
+    def get_formatted_date(self, params={}):
+        return self._get_formatted_date(params, 6)
+
+    @pp.apply_to(period='12monthly')
+    def get_formatted_date(self, params={}):
+        return self._get_formatted_date(params, 12)
+
+    def _get_formatted_date(self, params, range):
+        months = dateRange.getMonths(params['date'], range)
+        return "%s to %s" % (months[0].strftime('%B %Y'),
+                             months[-1].strftime('%B %Y'))
 
     def plot(self, outputFilename, **args):
         """
         Plot the thumbnail image and also the east and west map images.
         """
+
         variable = args['variable']
         area = args['area']
         period = args['period']
+        date = args['date']
 
-        dateObj = args['date']
-        date = dateObj.strftime('%Y%m%d')
-
-        formattedDate = ''
-        if variable == 'dec':
-            if period=='monthly':
-                formattedDate = dateObj.strftime('%B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + "decile/1950/" + period + "/" + "reynolds_sst_avhrr-only-v2_" + date[:6]  + "_dec"
-            else:
-                monthInt = ''.join(i for i in period if i.isdigit())
-                months = dateRange.getMonths(date, monthInt)
-                formattedDate = months[0].strftime('%B %Y') + ' to ' + months[-1].strftime('%B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + "decile/1950/" + period + "/" + \
-                           "reynolds_sst_avhrr-only-v2_" + monthInt + "mthavg_" + \
-                           months[0].strftime('%Y%m') + "_" + months[-1].strftime('%Y%m') + "_dec"
-        else:
-            if period=='daily':
-                formattedDate = dateObj.strftime('%d %B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + 'daily-new-uncompressed' + "/" + "avhrr-only-v2." + date
-            elif period=='predaily':
-                formattedDate = dateObj.strftime('%d %B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + period + "/" + date[:4] + "/" + "avhrr-only-v2." + date + "_preliminary"
-            elif period=='weekly':
-                weekdays = dateRange.getWeekDays(date)
-                formattedDate = weekdays[0].strftime('%d %B %Y') + ' to ' + weekdays[-1].strftime('%d %B %Y') 
-                spatialMean.generateWeekly(weekdays)
-                filename = self.serverCfg["dataDir"]["reynolds"] + period + "/" + "avhrr-only-v2." + weekdays[0].strftime('%Y%m%d') + "ave"
-            elif period=='monthly':
-                formattedDate = dateObj.strftime('%B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + "/averages/monthly/reynolds_sst_avhrr-only-v2_" + date[:6]
-            elif period=='3monthly':
-                months = dateRange.getMonths(date, 3)
-                formattedDate = months[0].strftime('%B %Y') + ' to ' + months[-1].strftime('%B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + \
-                           "/averages/3monthly/reynolds_sst_avhrr-only-v2_3mthavg_" + \
-                           months[0].strftime('%Y%m') + '_' + months[-1].strftime('%Y%m')
-            elif period=='6monthly':
-                months = dateRange.getMonths(date, 6)
-                formattedDate = months[0].strftime('%B %Y') + ' to ' + months[-1].strftime('%B %Y') 
-                filename = self.serverCfg["dataDir"]["reynolds"] + \
-                           "/averages/6monthly/reynolds_sst_avhrr-only-v2_6mthavg_" + \
-                           months[0].strftime('%Y%m') + '_' + months[-1].strftime('%Y%m')
-            elif period=='12monthly':
-                months = dateRange.getMonths(date, 12)
-                formattedDate = months[0].strftime('%B %Y') + ' to ' + months[-1].strftime('%B %Y')
-                filename = self.serverCfg["dataDir"]["reynolds"] + \
-                           "/averages/12monthly/reynolds_sst_avhrr-only-v2_12mthavg_" + \
-                           months[0].strftime('%Y%m') + '_' + months[-1].strftime('%Y%m')
-            elif period=='yearly':
-                formattedDate = date[:4]
-                filename = self.serverCfg["dataDir"]["reynolds"] + period + "/avhrr-only-v2." + date[:4] + "ave"
-            else:
-                return -1
+        formattedDate = self.get_formatted_date(params=args)
 
         cb_labels = None
         cb_label_pos = None
@@ -106,23 +118,26 @@ class ReynoldsPlotter ():
             else:
                 cb_ticks = np.arange(0.0,32.1,2.0)
 
-        if variable == 'anom':
+        elif variable == 'anom':
             extend = 'both'
             cb_tick_fmt="%.1f"
             cb_ticks = np.arange(-2.0,2.01,0.5)
 
-        if variable == 'dec':
+        elif variable == 'dec':
             extend = 'neither'
             cb_tick_fmt="%.1f"
             cb_ticks = np.arange(0.5,7.51,1)
-            cb_labels=['Lowest on \nrecord','Very much \nbelow average \n[1]','Below average \n[2-3]','Average \n[4-7]','Above average \n[8-9]','Very much \nabove average \n[10]','Highest on \nrecord']
-            #cb_labels=['Very much \nbelow average \n[1]','Below average \n[2-3]','Average \n[4-7]','Above average \n[8-9]','Very much \nabove average \n[10]']
+            cb_labels=['Lowest on \nrecord',
+                       'Very much \nbelow average \n[1]',
+                       'Below average \n[2-3]',
+                       'Average \n[4-7]',
+                       'Above average \n[8-9]',
+                       'Very much \nabove average \n[10]',
+                       'Highest on \nrecord']
             cb_label_pos=[1.0,2.0,3.0,4.0,5.0,6.0,7.0]
 
         args['formattedDate'] = formattedDate
-        filename = filename + ".nc"
-
-        output_filename = self.serverCfg["outputDir"] + outputFilename + '.png'
+        output_filename = self.serverCfg['outputDir'] + outputFilename + '.png'
 
         regionLongName = regionConfig.regions[area][2]
         title = regionLongName + '\n'
@@ -146,12 +161,16 @@ class ReynoldsPlotter ():
         lon_min = regionConfig.regions[area][1]['llcrnrlon']
         lon_max = regionConfig.regions[area][1]['urcrnrlon']
 
-        lats, lons, _, sst = \
-            branPlotterNew.load_BRAN_data(filename, self.config.getVariableType(variable),
-                                          lat_min - 1.0, lat_max + 1.0,
-                                          lon_min - 1.0, lon_max + 1.0)
+        gridvar = self.config.getVariableType(variable)
 
-        plot.plot_surface_data(lats, lons, sst,
+        grid = Gridset(self.get_path(params=args), gridvar, period,
+                       prefix=self.get_prefix(params=args),
+                       suffix=self.get_suffix(params=args),
+                       date=date,
+                       lonrange=(lon_min, lon_max),
+                       latrange=(lat_min, lat_max))
+
+        plot.plot_surface_data(grid.lats, grid.lons, grid.data,
                                lat_min, lat_max, lon_min, lon_max,
                                output_filename=output_filename,
                                title=title, units=units,
@@ -162,18 +181,23 @@ class ReynoldsPlotter ():
                                product_label_str='Reynolds SST',
                                area=area)
 
-        lats, lons, _, sst = \
-            branPlotterNew.load_BRAN_data(filename, self.config.getVariableType(variable), -999.0, 999.0, -999.0, 999.0)
+        grid = Gridset(self.get_path(params=args), gridvar, period,
+                       prefix=self.get_prefix(params=args),
+                       suffix=self.get_suffix(params=args),
+                       date=date)
 
         if variable == 'dec':
-            # Mask out polar region to avoid problem of calculating deciles over sea ice
-            sst.mask[0:bisect.bisect_left(lats,-60),:] = True
-            sst.mask[bisect.bisect_left(lats,60):-1,:] = True
+            # Mask out polar region to avoid problem of calculating deciles
+            # over sea ice
+            grid.data.mask[0:bisect.bisect_left(grid.lats,-60),:] = True
+            grid.data.mask[bisect.bisect_left(grid.lats,60):-1,:] = True
 
-        plot.plot_basemaps_and_colorbar(lats, lons, sst,
+        plot.plot_basemaps_and_colorbar(grid.lats, grid.lons, grid.data,
                                         output_filename=output_filename,
-                                        units=units, cm_edge_values=cb_ticks, cb_tick_fmt=cb_tick_fmt,
-                                        cb_labels=cb_labels, cb_label_pos=cb_label_pos,
+                                        units=units, cm_edge_values=cb_ticks,
+                                        cb_tick_fmt=cb_tick_fmt,
+                                        cb_labels=cb_labels,
+                                        cb_label_pos=cb_label_pos,
                                         cmp_name=cmap_name, extend=extend)
 
         plot.wait()
