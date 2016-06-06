72054554fc89900a1808bbe0aefcb677e02350c0
Sheng Guo <s.guo@bom.gov.au>
2016-06-06 19:50:29 +1000

    add missing colors for outlook. Bug#798

--- ocean/datasets/coral/coral.py
+++ ocean/datasets/coral/coral.py
@@ -126,6 +126,14 @@ class CoralPlotterWrapper(SurfacePlotter):
                          [240, 0, 0],
                          [150, 0, 0]])
 
+    @apply_to(variable='outlook')
+    def get_basemap_colors(self, params={}):
+        return np.array([[255, 255, 255],
+                         [191, 191, 191],
+                         [128, 128, 128],
+                         [64, 64, 64],
+                         [0, 0, 0]])
+
     def get_fill_color(self, params={}):
         #return '0.59'
         return '0.98'
